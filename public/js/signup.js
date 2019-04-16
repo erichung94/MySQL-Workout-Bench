91d1f64f0adf544ae1ab82183eac07b2d149755e
Mary Dang <dangmaryk@gmail.com>
2019-04-16 13:01:48 -0500

    changed matt's signup page to handlebars

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
+    console.log("submitted");
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
