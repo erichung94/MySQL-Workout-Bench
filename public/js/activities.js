78a3bd857a21c45ee875887e5c8fa0fba9bff4e1
Matthew Trabold <mct4240@gmail.com>
2019-04-17 12:23:01 -0500

    Updated the js

--- public/js/activities.js
+++ public/js/activities.js
@@ -1,13 +1,27 @@
 var prevId;
 
 $(".activity-square").on("click", function() {
-  $(this).css({
-    'background-color': 'white'
-  });
+
+
+    $(this).css({
+      'background-color': '#33CCFF',
+      'transition-duration' : '.5s',
+      'transition-property' : 'background-color'
+    });
+    $(this).find('img').attr('src', 'images/activityIcons/' + $(this).attr('id') + '-white.png');
+    $(this).find('p').css({
+      'color' : 'white',
+    });
+
+
   if (prevId) {
     $("#" + prevId).css({
-      'background-color' : '#333c48'
-    })
+      'background-color' : 'white'
+    });
+    $('#' + prevId + " > img").attr('src', 'images/activityIcons/' + prevId + '.png');
+    $('#' + prevId + " > p").css({
+      'color' : '#242B33'
+    });
   }
 
   prevId = $(this).attr('id');
