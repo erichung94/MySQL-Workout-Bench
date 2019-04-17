1933b3b6139a2981823a164f1f01098ac5bb4e46
Lynn Tang <tanglynn2@gmail.com>
2019-04-17 12:16:58 -0500

    before fixing Mary's code

--- models/user.js
+++ models/user.js
@@ -40,12 +40,12 @@ module.exports = function(sequelize, DataTypes) {
     User.hook("beforeCreate", function(user) {
         user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
     });
+  
+    // Associating Users with Workouts
+    User.associate = function(models){
+        User.belongsToMany(models.Workout, {
+            through: "UserWorkout" });
+    };
+      
     return User;
 };
-
-// Associating Users with Workouts
-User.associate = function(models){
-    User.belongsToMany(models.Workout, {
-        through: "UserWorkout" });
-};
-  
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,34 +7,6 @@ module.exports = function(app) {
     //
     // ******************************************************************************
 
-<<<<<<< HEAD
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    console.log("helloooooo");
-    console.log(req.body);
-    // The redirect will happen on the front end
-    res.json("/profile");
-  });
-
-  // Route for signing up a user. The user's password is automatically hashed and stored securely
-  // If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
-    console.log("YOU ARE HEREEEEEE");
-    console.log(req.body);
-    db.User.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password
-    }).then(function() {
-      res.redirect(307, "/api/login");
-    }).catch(function(err) {
-      console.log(err);
-      res.json(err);
-      // res.status(422).json(err.errors[0].message);
-=======
     // If the user has valid login credentials, send them to the members page.
     // Otherwise the user will be sent an error
     app.post("/api/login", passport.authenticate("local"), function(req, res) {
@@ -59,7 +31,6 @@ module.exports = function(app) {
             res.json(err);
             // res.status(422).json(err.errors[0].message);
         });
->>>>>>> master
     });
 
     // Route for logging user out
@@ -68,25 +39,6 @@ module.exports = function(app) {
         res.redirect("/");
     });
 
-<<<<<<< HEAD
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    }
-    else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
-=======
     // Route for getting some data about our user to be used client side
     app.get("/api/user_data", function(req, res) {
         if (!req.user) {
@@ -102,7 +54,14 @@ module.exports = function(app) {
             });
         }
     });
->>>>>>> master
 
 };
 
+
+///// LOGIC FOR MATCHING ////
+db.User.findAll({
+    include: [
+        { model: db.Workout, where: { activity: "cardio", time: "evening" } }
+    ]
+})
+    .then(results => results.map(v => console.log(v.Workouts.map(c => c.dataValues.activity), v.dataValues.id)));
